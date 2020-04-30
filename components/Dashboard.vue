7b76ca5555b64f459d12db8c32b7993a4f51e397
Monty Anderson <work@montyanderson.net>
2020-04-30 22:01:25 +0100

    dashboard components

--- components/Dashboard.vue
+++ components/Dashboard.vue
@@ -10,17 +10,35 @@
 			<h2 class="gateway-title">Gateway Config</h2>
 
 			<p class="access-key-label">Access Key</p>
-			<p class="access-key-show">Show</p>
+
+			<p class="access-key-show" v-on:click="accessKeyShown = true" v-if="accessKeyShown === false">Show <img src="resources/img/show.png"></p>
+			<p class="access-key-hide" v-on:click="accessKeyShown = false" v-if="accessKeyShown === true">Hide <img src="resources/img/hide.png"></p>
+
 			<input v-bind:type="accessKeyInputType" disabled class="access-key" v-model="accessKey">
 
 			<p class="secret-key-label">Secret Key</p>
+
+			<p class="secret-key-show" v-on:click="secretKeyShown = true" v-if="secretKeyShown === false">Show <img src="resources/img/show.png"></p>
+			<p class="secret-key-hide" v-on:click="secretKeyShown = false" v-if="secretKeyShown === true">Hide <img src="resources/img/hide.png"></p>
+
 			<input v-bind:type="secretKeyInputType" disabled class="secret-key" v-model="secretKey">
 		</div>
 
 		<div class="bucket-box">
+			<div class="reconfigure">
+				<router-link to="/wizard">Reconfigure</router-link>
+			</div>
+
 			<h2 class="bucket-title">Bucket Details</h2>
 
+			<p class="satellite-label">Satellite</p>
+			<input class="satellite" type="text" disabled v-model="satellite">
 
+			<p class="api-key-label">API Key</p>
+			<input class="api-key" type="password" disabled value="****************">
+
+			<p class="passphrase-label">Encryption Passphrase</p>
+			<input class="passphrase" type="password" disabled value="****************">
 		</div>
 	</div>
 </template>
@@ -32,7 +50,9 @@ module.exports = {
 		accessKeyShown: true,
 
 		secretKey: '2sHDQ6n8rPLuhBve8aaWrR3Grq55',
-		secretKeyShown: false
+		secretKeyShown: false,
+
+		satellite: 'us-central-1.tardigrade.io'
 	}),
 	computed: {
 		accessKeyInputType() {
@@ -152,8 +172,43 @@ module.exports = {
 	color: #384B65;
 }
 
-.access-key-show {
-	
+.access-key-show, .access-key-hide {
+	position: absolute;
+	left: 315px;
+	top: 96px;
+
+	width: 69px;
+	height: 14px;
+
+	font-family: Inter;
+	font-style: normal;
+	font-weight: normal;
+	font-size: 14px;
+	line-height: 100%;
+	/* identical to box height, or 14px */
+
+	display: flex;
+	align-items: center;
+
+	color: #9BA4B2;
+
+	cursor: pointer;
+}
+
+.access-key-show img {
+	position: absolute;
+	left: 46px;
+	top: 0px;
+
+	width: 20px;
+}
+
+.access-key-hide img {
+	position: absolute;
+	left: 46px;
+	top: -3px;
+
+	width: 20px;
 }
 
 .access-key {
@@ -226,6 +281,45 @@ module.exports = {
 	text-indent: 20px;
 }
 
+.secret-key-show, .secret-key-hide {
+	position: absolute;
+	left: 315px;
+	top: 201px;
+
+	width: 69px;
+	height: 14px;
+
+	font-family: Inter;
+	font-style: normal;
+	font-weight: normal;
+	font-size: 14px;
+	line-height: 100%;
+	/* identical to box height, or 14px */
+
+	display: flex;
+	align-items: center;
+
+	color: #9BA4B2;
+
+	cursor: pointer;
+}
+
+.secret-key-show img {
+	position: absolute;
+	left: 46px;
+	top: 0px;
+
+	width: 20px;
+}
+
+.secret-key-hide img {
+	position: absolute;
+	left: 46px;
+	top: -3px;
+
+	width: 20px;
+}
+
 .bucket-box {
 	position: absolute;
 	left: 684px;
@@ -236,6 +330,31 @@ module.exports = {
 
 	background: #FEFEFF;
 	border-radius: 6px;
+}
+
+.reconfigure {
+	position: absolute;
+	left: 352px;
+	top: 74px;
+
+	width: 127px;
+	height: 14px;
+
+	font-family: Inter;
+	font-style: normal;
+	font-weight: normal;
+	font-size: 14px;
+	line-height: 100%;
+	/* identical to box height, or 14px */
+
+	display: flex;
+	align-items: center;
+}
+
+.reconfigure > * {
+	width: 127px;
+	text-align: right !important;
+	color: #2683FF !important;
 }
 
 .bucket-title {
@@ -255,5 +374,140 @@ module.exports = {
 	align-items: center;
 
 	color: #0D1826;
+}
+
+.satellite-label {
+	position: absolute;
+	left: 72px;
+	top: 119px;
+
+	width: 61px;
+	height: 16px;
+
+	font-family: Inter;
+	font-style: normal;
+	font-weight: normal;
+	font-size: 16px;
+	line-height: 100%;
+	/* identical to box height, or 16px */
+
+	display: flex;
+	align-items: center;
+
+	color: #384B65;
+}
+
+.satellite {
+	position: absolute;
+	left: 72px;
+	top: 143px;
+
+	width: 407px;
+	height: 52px;
+
+	background: rgba(226, 229, 233, 0.4);
+	border: 1px solid #E2E6E9;
+	box-sizing: border-box;
+	border-radius: 10px;
+
+	font-family: Inter;
+	font-style: normal;
+	font-weight: normal;
+	font-size: 16px;
+	line-height: 134.09%;
+
+	text-indent: 20px;
+
+	color: rgba(56, 75, 101, 0.75);
+}
+
+.api-key-label {
+	position: absolute;
+	left: 72px;
+	top: 223px;
+
+	width: 58px;
+	height: 16px;
+
+	font-family: Inter;
+	font-style: normal;
+	font-weight: normal;
+	font-size: 16px;
+	line-height: 100%;
+	/* identical to box height, or 16px */
+
+	display: flex;
+	align-items: center;
+
+	color: #384B65;
+}
+
+.api-key {
+	position: absolute;
+	left: 72px;
+	top: 247px;
+
+	width: 407px;
+	height: 52px;
+
+	background: rgba(226, 229, 233, 0.4);
+	border: 1px solid #E2E6E9;
+	box-sizing: border-box;
+	border-radius: 10px;
+
+	font-family: Inter;
+	font-style: normal;
+	font-weight: normal;
+	font-size: 16px;
+	line-height: 134.09%;
+
+	text-indent: 20px;
+
+	color: rgba(56, 75, 101, 0.75);
+}
+
+.passphrase-label {
+	position: absolute;
+	left: 73px;
+	top: 331px;
+
+	width: 174px;
+	height: 16px;
+
+	font-family: Inter;
+	font-style: normal;
+	font-weight: normal;
+	font-size: 16px;
+	line-height: 100%;
+	/* identical to box height, or 16px */
+
+	display: flex;
+	align-items: center;
+
+	color: #384B65;
+}
+
+.passphrase {
+	position: absolute;
+	left: 73px;
+	top: 355px;
+
+	width: 407px;
+	height: 52px;
+
+	background: rgba(226, 229, 233, 0.4);
+	border: 1px solid #E2E6E9;
+	box-sizing: border-box;
+	border-radius: 10px;
+
+	font-family: Inter;
+	font-style: normal;
+	font-weight: normal;
+	font-size: 16px;
+	line-height: 134.09%;
+
+	text-indent: 20px;
+
+	color: rgba(56, 75, 101, 0.75);
 }
 </style>
