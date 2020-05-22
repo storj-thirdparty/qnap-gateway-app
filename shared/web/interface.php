ea9f3bbb7cc8f7523d7ff05028d5223190594e38
Mrityunjay <kmrityunjay@gmail.com>
2020-05-22 20:29:35 +0530

    Updated & refactored  API endpoint

--- shared/web/interface.php
+++ shared/web/interface.php
@@ -3,66 +3,72 @@
 	$arr = array ();
 	$data = json_decode(file_get_contents("php://input"), TRUE);
 	$jsondata = ReadJSON($file);
+	$apiRequest = "";
 
 	// Endpoint to return the dashboard info to wizard
-	if(!array_key_exists('apiRequest', $data))  {
-	    // Do error Handling and return
-	    return ;
-	} else {
-	   // API Request can be any of 
-	   // 1) dashboard-info
-	   // 2) 
-           $apiRequest = $data['apiRequest'];
-	}
-	
-	if ($apiRequest === 'dashboard-info') {
-		$arr = array ( 
-		        "accessKey" => $jsondata['AccessKey'], 
-		        "secretKey" => $jsondata['SecretKey'],
-		        "satellite" => $jsondata['Satellite'],
-		); 
-  
-		echo json_encode($arr); 
-	}
-	// Endpoint to save the parameters and configuring the gateway
-	else if ($apiRequest === 'wizard-save') {
-		if(
-		    $data['satellite'] == null  || $data['satellite'] == "" ||
-		    $data['apiKey'] == null     || $data['apiKey'] == "" ||
-		    $data['passphrase'] == null || $data['passphrase'] =="" )
-		{
+	if($data['apiRequest'] == "dashboard-info" || $data['apiRequest'] == "wizard-save" || $data['apiRequest'] == "gateway-action")  
+	{
+		$apiRequest = $data['apiRequest'];
 
-		    $arr = array ( "Error" => "Kindly check inputs vlues!"); 
-			echo json_encode($arr); 
-		}else{
-		    $satellite = $data['satellite'];
-		    $apiKey = $data['apiKey'];
-		    $passphrase = $data['passphrase'];
+		if ($apiRequest === 'dashboard-info') {
+			$arr = array ( 
+			        "accessKey" => $jsondata['AccessKey'], 
+			        "secretKey" => $jsondata['SecretKey'],
+			        "satellite" => $jsondata['Satellite'],
+			); 
+	  
 		}
-	}
+		// Endpoint to save the parameters and configuring the gateway
+		else if ($apiRequest === 'wizard-save') {
+			if(isset($data['satellite']) && isset($data['apiKey']) && isset($data['passphrase'])){
+				if(
+				    $data['satellite'] == null  || $data['satellite'] == "" ||
+				    $data['apiKey'] == null     || $data['apiKey'] == "" ||
+				    $data['passphrase'] == null || $data['passphrase'] =="" )
+				{
 
-	// Endpoint to send commands to gateway
-	else if ( $apiRequest === 'gateway-action') {
-		if($data['action'] == null || $data['action'] == "" ) {
-			$arr = array ( "error" => "Kindly check action!"); 
-			
-		}else{
-			if($data['action'] == "run"){
+				    $arr = array ( "error" => "Invalid or Unknown API Request"); 
+				}else{
+				    $satellite = $data['satellite'];
+				    $apiKey = $data['apiKey'];
+				    $passphrase = $data['passphrase'];
 
-				$arr = array ( "status" => "running"); 
-
-			}else if($data['action'] == "stop"){
-
-				$arr = array ( "status" => "stopped"); 
-			}else if($data['action'] == "restart"){
-
-				$arr = array ( "status" => "restarting"); 
+				}
+			}else{
+				$arr = array ( "error" => "Invalid or Unknown API Request"); 
 			}
 		}
 
-		echo json_encode($arr); 
+		// Endpoint to send commands to gateway
+		else if ( $apiRequest === 'gateway-action') {
+			if(isset($data['action'])){
+				if($data['action'] == null || $data['action'] == "" ) {
+					$arr = array ( "error" => "Invalid or Unknown API Request"); 
+					
+				}else{
+					if($data['action'] == "run"){
 
+						$arr = array ( "status" => "running"); 
+
+					}else if($data['action'] == "stop"){
+
+						$arr = array ( "status" => "stopped"); 
+					}else if($data['action'] == "restart"){
+
+						$arr = array ( "status" => "restarting"); 
+					}
+				}
+			}else{
+				$arr = array ( "error" => "Invalid or Unknown API Request"); 
+			}
+
+
+		}
+	}else{
+		$arr = array ( "error" => "Invalid or Unknown API Request"); 
 	}
+
+	echo json_encode($arr); 
 
 	function ReadJSON($file)
 	{
